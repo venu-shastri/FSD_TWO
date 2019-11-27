447fddaaf3835b15dc826339c83eb73c8af899ee
venu-shastri <venu.shastri@gmail.com>
2019-11-27 11:41:25 +0530

    Create serviceLocatior.js

---
+++ serviceLocatior.js
@@ -0,0 +1,22 @@
+  const dependencies = {};
+  const factories = {};
+  const serviceLocator = {};
+  
+  serviceLocator.factory = (name, factory) => {
+    factories[name] = factory;
+  };
+  
+  serviceLocator.register = (name, instance) => {
+    dependencies[name] = instance;
+  };
+  
+  serviceLocator.get = (name) => {
+    if (!dependencies[name]) {
+      const factory = factories[name];
+      dependencies[name] = factory && factory(serviceLocator);
+      if (!dependencies[name]) {
+        throw new Error('Cannot find module: ' + name);
+      }
+    }
+    return dependencies[name];
+  };
